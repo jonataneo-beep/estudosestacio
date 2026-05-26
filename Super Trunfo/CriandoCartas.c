36f8630ceeab61b0da5a29a98f5bfe0a54eedce1
jonataneo-beep <jonataneo@hotmail.com>
2026-05-26 12:45:28 +0000

    Calculo da Densidade Populacional e Renda Per Capi

--- Super Trunfo/CriandoCartas.c
+++ Super Trunfo/CriandoCartas.c
@@ -6,6 +6,9 @@ int main(){
     char estado[8];
     char codigocarta[3];
     char cidade[50];
+    float densidade;
+    float perCapita;
+
     //Entrada de dados para cadastro da primeira carta
         printf("Estado: \n");
         scanf("%s", &estado);
@@ -27,10 +30,16 @@ int main(){
 
         printf("Turisticos: \n");
         scanf("%d", &turisticos);
+
+//Calculo da densidade populacional e PIB per Capita
+
+densidade = (float) populacao / area;
+perCapita = (float) (pib / populacao) * 1000000000;
+
 //Retorno dos dados da primeira carta criada
-        printf("Carta Criada: 1 %s\n Estado: %s\n Codigo da Carta: %s\n Cidade: %s\n Populacao: %d Milhoes de Pessoas\n Area: %.4f Km2\n PIB: %.4f Bilhoes de Reais\n Numero de Pontos Turisticos: %d\n", estado, estado, codigocarta, cidade, populacao, area, pib, turisticos);
+        printf("Carta Criada: 1 %s\n Estado: %s\n Codigo da Carta: %s\n Cidade: %s\n Populacao: %d Milhoes de Pessoas\n Area: %.2f Km2\n PIB: %.2f Bilhoes de Reais\n Numero de Pontos Turisticos: %d\n Densidade Populacional: %.3f hab/km2\n PIB per Capita: %.3f reais\n", estado, estado, codigocarta, cidade, populacao, area, pib, turisticos, densidade, perCapita);
        
- //Entrada de dados para cadastro da Segunda carta
+//Entrada de dados para cadastro da Segunda carta
         printf("Estado: \n");
         scanf("%s", &estado);
 
@@ -51,7 +60,13 @@ int main(){
 
         printf("Turisticos: \n");
         scanf("%d", &turisticos);
+
+//Calculo da densidade populacional e PIB per Capita
+
+densidade = (float) populacao / area;
+perCapita = (float) (pib / populacao) * 1000000000;
+
 //Retorno dos dados da segunda carta criada
-        printf("Carta Criada: 2 %s\n Estado: %s\n Codigo da Carta: %s\n Cidade: %s\n Populacao: %d Milhoes de Pessoas\n Area: %.4f Km2\n PIB: %.4f Bilhoes de Reais\n Numero de Pontos Turisticos: %d\n", estado, estado, codigocarta, cidade, populacao, area, pib, turisticos);
-return 0;
+        printf("Carta Criada: 2 %s\n Estado: %s\n Codigo da Carta: %s\n Cidade: %s\n Populacao: %d Milhoes de Pessoas\n Area: %.2f Km2\n PIB: %.2f Bilhoes de Reais\n Numero de Pontos Turisticos: %d\n Densidade Populacional: %.3f hab/km2\n PIB per Capita: %.3f reais\n", estado, estado, codigocarta, cidade, populacao, area, pib, turisticos, densidade, perCapita);
+     return 0;
 }
