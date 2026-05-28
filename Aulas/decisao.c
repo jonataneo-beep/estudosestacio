3ac1e3a9333e5a2c3374c21d292d8ff60eab70d3
jonataneo-beep <jonataneo@hotmail.com>
2026-05-28 02:58:33 +0000

    decisão

--- Aulas/decisao.c
+++ Aulas/decisao.c
@@ -1,16 +1,31 @@
 #include <stdio.h>
 
 int main() {
-    int idade;
+    float temperatura, umidade;
+    unsigned int estoque, estoqueMinimo = 1000;
 
-    printf("Digite a sua idade: ");
-    scanf("%d", &idade);
+    printf("Entre com a temperatura:\n");
+    scanf("%f", &temperatura);
+    printf("Entre com a umidade:\n");
+    scanf("%f", &umidade);
+    printf("Entre com o estoque:\n");
+    scanf("%u", &estoque);
 
-    if (idade >= 18) {
-        printf("Você é maior de idade.\n");
+    if (temperatura > 30) {
+        printf("Temperatura alta detectada.\n");
     } else {
-        printf("Você é menor de idade.\n");
+        printf("Temperatura normal.\n");
     }
 
+    if (umidade > 50) {
+        printf("Umidade alta detectada.\n");
+    } else {
+        printf("Umidade normal.\n");
+    }
+    if (estoque < estoqueMinimo) {
+        printf("Estoque abaixo do mínimo.\n");
+    } else {
+        printf("Estoque suficiente.\n");
+    }
     return 0;
 }
