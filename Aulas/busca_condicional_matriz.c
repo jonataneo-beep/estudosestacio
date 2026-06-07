c6d825235b6e7bb5c47c62fd0997efdbe0a2d278
jonataneo-beep <jonataneo@hotmail.com>
2026-06-07 01:18:54 +0000

    Batalha Naval Nivel Mestre

--- Aulas/busca_condicional_matriz.c
+++ Aulas/busca_condicional_matriz.c
@@ -1,13 +1,29 @@
 #include <stdio.h>
+
+#define LINHA 10
+#define COLUNA 10
+
  
 int main() {
-    int matriz[3][3] = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
-    int target = 5;
+    int matriz[LINHA][COLUNA];
+    int target = 90;
     int found = 0;
+    int soma = 1;
+
+    for (int i = 0; i < LINHA; i++) {      // Loop externo para as linhas
+        for (int j = 0; j < COLUNA; j++) {
+            matriz[i][j] = soma;
+            soma++;
+            printf("%d  ", matriz[i][j]);
+        }
+        printf("\n");
+    }    
+
+
  
     // Busca condicional do elemento alvo
-    for (int i = 0; i < 3; i++) {      // Loop externo para as linhas
-        for (int j = 0; j < 3; j++) {  // Loop interno para as colunas
+    for (int i = 0; i < LINHA; i++) {      // Loop externo para as linhas
+        for (int j = 0; j < COLUNA; j++) {  // Loop interno para as colunas
             if (matriz[i][j] == target) {
                 printf("Elemento %d encontrado na posição (%d, %d)\n", target, i, j);
                 found = 1;
--- Batalha_Naval/Batalha_Naval.c
+++ Batalha_Naval/Batalha_Naval.c
@@ -1,56 +1,94 @@
 #include <stdio.h>
+#include <stdlib.h>
+
+#define LINHA 10
+#define COLUNA 10
+
+void desenharOctaedro(int tabuleiro[LINHA][COLUNA], int centroLinha, int centroColuna, int raio){
+    for (int i = 0; i < LINHA; i++){
+        for (int j = 0; j < COLUNA; j++){
+            if (abs(i - centroLinha) + abs(j - centroColuna) <= raio){
+                tabuleiro[i][j] = 2;
+            }
+        }
+    }
+}
+void desenharCone(int tabuleiro[LINHA][COLUNA], int linhaInicial, int colunaCentral, int altura){
+    for (int i = 0; i < LINHA; i++){
+        for (int j = 0; j < COLUNA; j++){
+            if (i >= linhaInicial && i < linhaInicial + altura && j >= colunaCentral - (i - linhaInicial) && j <= colunaCentral + (i - linhaInicial)){
+                tabuleiro[i][j] = 1;
+            }
+        }
+    }
+}
+void desenharCruz(int tabuleiro[LINHA][COLUNA], int centroLinha, int centroColuna, int tamanho){
+    for (int i = 0; i < LINHA; i++){
+        for (int j = 0; j < COLUNA; j++){
+            if ((i == centroLinha && j >= centroColuna - tamanho && j <= centroColuna + tamanho) || (j == centroColuna && i >= centroLinha - tamanho && i <= centroLinha + tamanho)) {
+                tabuleiro[i][j] = 3;
+            }
+        }
+    }
+}
+
+
 
 int main() {
     char linha[10]= {'A','B','C','D','E','F','G','H','I','J'};
     // 1. Criando o Tabuleiro (Matriz 10x10)
-    int tabuleiro[10][10];
-    for (int i = 0; i < 10; i++) {
-        for (int j = 0; j < 10; j++) {
+    int tabuleiro[LINHA][COLUNA];
+    for (int i = 0; i < LINHA; i++) {
+        for (int j = 0; j < COLUNA; j++) {
             tabuleiro[i][j] = 0; // Inicializa o tabuleiro com 0 (água)
         }
     }
     // 2. Exibindo o Tabuleiro
     printf("  ");
-   for (int i = 0; i < 10; i++) {
-        printf("%c ", linha[i]);
+   for (int i = 0; i < LINHA; i++) {
+        printf("  %c", linha[i]);
     }
     printf("\n");
-    for (int i = 1; i <= 10; i++) {
+    for (int i = 1; i <= LINHA; i++) {
         printf("%d ", i);
-        for (int j = 0; j < 10; j++) {
+        for (int j = 0; j < COLUNA; j++) {
             printf("%d ", tabuleiro[i-1][j]);
         }
         printf("\n");
     }
 // Inserir navios no tabuleiro
 // Navio na vertical (F2, G2, H2)
-    for (int i = 0; i < 3; i++) {
-        tabuleiro[i][5] = 3; // Navio na vertical (F2, G2, H2)
-    }
+//    for (int i = 0; i < 3; i++) {
+//        tabuleiro[i][5] = 3; // Navio na vertical (F2, G2, H2)
+//    }
 // Navio na horizontal (A1, A2, A3)
-    for (int i = 0; i < 3; i++) {
-        tabuleiro[0][i] = 3; // Navio na horizontal (A1, A2, A3)
-    }    
+//    for (int i = 0; i < 3; i++) {
+//        tabuleiro[0][i] = 3; // Navio na horizontal (A1, A2, A3)
+//    }    
 //Navio na diagonal (A1, B2, C3, D4, E5, F6, G7, H8, I9, J10)    
-    for (int i = 7; i < 10; i++) {
-        tabuleiro[i][i] = 3; // Navio na diagonal (A1, B2, C3, D4, E5, F6, G7, H8, I9, J10)
-    } 
+//    for (int i = 7; i < 10; i++) {
+//        tabuleiro[i][i] = 3; // Navio na diagonal (A1, B2, C3, D4, E5, F6, G7, H8, I9, J10)
+//    } 
 //Navio na diagonal (A10, B9, C8, D7, E6, F5, G4, H3, I2, J1)    
-    for (int i = 6; i < 9; i++) {
-        tabuleiro[i][9 - i] = 3; // Navio na diagonal (A10, B9, C8, D7, E6, F5, G4, H3, I2, J1)
-    }
+//    for (int i = 6; i < 9; i++) {
+//        tabuleiro[i][9 - i] = 3; // Navio na diagonal (A10, B9, C8, D7, E6, F5, G4, H3, I2, J1)
+//    }
+desenharOctaedro(tabuleiro, 6, 6, 2);
+desenharCone(tabuleiro, 2, 2, 3);
+//Valores Linha, Coluna e Tamanho
+desenharCruz(tabuleiro, 2, 5, 1);
 
 
 // Exibir o tabuleiro atualizado
     printf("\nTabuleiro atualizado:\n");
     printf("  ");
-    for (int i = 0; i < 10; i++) {
+    for (int i = 0; i < LINHA; i++) {
         printf("%c ", linha[i]);
     }
     printf("\n");
-    for (int i = 1; i <= 10; i++) {
+    for (int i = 1; i <= LINHA; i++) {
         printf("%d- ", i);
-        for (int j = 0; j < 10; j++) {
+        for (int j = 0; j < COLUNA; j++) {
             printf("%d ", tabuleiro[i-1][j]);
         }
     printf("\n");
